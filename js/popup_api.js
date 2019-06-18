ac0ec21d4ba3197d956c1e3b07ee7d27e8bad811
Jay Sharma <jy741228@dal.ca>
2019-06-18 20:58:22 -0300

    Replaced and added alpha channel to PNG banners

--- js/popup_api.js
+++ js/popup_api.js
@@ -3,13 +3,11 @@ function popup(city) {
 		var city2 = document.getElementById("search_location").value;
 		if (city2==''){}
 		else{
-			alert("Searching Food Banks near " + city2);
 			document.getElementById("search_location").value=city2;
 			document.getElementById("locate").click();
 		}
 	}
 	else{
-		alert("Showing search results from " + city);
 		document.getElementById("search_location").value=city+", Canada";
 		document.getElementById("locate").click();
 	}
