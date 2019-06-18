ca91ef0819681b2d0e668552ec364034684b681f
Jonathan.Harris <j.wilfred.harris@gmail.com>
2019-06-18 12:32:38 -0300

    Added separate js files for donorProfile.html. Update information functionality on donor_profile.html now working

--- js/login_api.js
+++ js/login_api.js
@@ -1,3 +1,10 @@
+
+// This site was helpful in determining how to work with javascript login logic.
+// https://www.w3schools.com/js/js_validation.asp
+// This link was helpful in determining how to complete a javascript redirect.
+// https://www.w3schools.com/howto/howto_js_redirect_webpage.asp
+// This site was helpful in determining how to make javascript functions run against form content.
+// https://www.tutorialspoint.com/javascript/javascript_form_validations.htm
 function login(){
 
 	var usernameVar = document.loginForm.username.value;
---
+++ js/updatePassword_api.js
@@ -0,0 +1,8 @@
+// This link was helpful re: the string.repeat() method:
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+function updatePassword(){
+	var newPassword = prompt("Please enter new password.", "");
+	if((newPassword != null) && (newPassword != "")){
+		document.getElementById("password").placeholder = "*".repeat(newPassword.length);
+	}
+}
