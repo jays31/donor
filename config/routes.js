9d25efe3cbe7f07bf009ac6848879c9711453b81
Jay Sharma <jy741228@dal.ca>
2019-07-10 14:12:46 -0300

    Configured page routing

--- config/routes.js
+++ config/routes.js
@@ -1,44 +1,14 @@
-/**
- * Route Mappings
- * (sails.config.routes)
- *
- * Your routes tell Sails what to do each time it receives a request.
- *
- * For more information on configuring custom routes, check out:
- * https://sailsjs.com/anatomy/config/routes-js
- */
+// Author: Jay Sharma (B00824331)
 
 module.exports.routes = {
 
-  /***************************************************************************
-  *                                                                          *
-  * Make the view located at `views/homepage.ejs` your home page.            *
-  *                                                                          *
-  * (Alternatively, remove this and add an `index.html` file in your         *
-  * `assets` directory)                                                      *
-  *                                                                          *
-  ***************************************************************************/
-
   '/': { view: 'pages/homepage' },
-  'GET': { view: 'pages/about' },
-  'GET': { view: 'pages/dashboard' },
-  'GET': { view: 'pages/donor_profile' },
-  'GET': { view: 'pages/forgot' },
-  'GET': { view: 'pages/help' },
-  'GET': { view: 'pages/inventory' },
-  'GET': { view: 'pages/signup' },
-
-
-  /***************************************************************************
-  *                                                                          *
-  * More custom routes here...                                               *
-  * (See https://sailsjs.com/config/routes for examples.)                    *
-  *                                                                          *
-  * If a request to a URL doesn't match any of the routes in this file, it   *
-  * is matched against "shadow routes" (e.g. blueprint routes).  If it does  *
-  * not match any of those, it is matched against static assets.             *
-  *                                                                          *
-  ***************************************************************************/
-
+  '/about': { view: 'pages/about' },
+  '/dashboard': { view: 'pages/dashboard' },
+  '/donor_profile': { view: 'pages/donor_profile' },
+  '/forgot': { view: 'pages/forgot' },
+  '/help': { view: 'pages/help' },
+  '/inventory': { view: 'pages/inventory' },
+  '/signup': { view: 'pages/signup' },
 
 };
