91ed7f2c5837fc3927f511ac3be9fe940c11c573
Jonathan.Harris <j.wilfred.harris@gmail.com>
2019-07-17 20:56:56 -0300

    Changed database query to affect a copy of itemlist table - itemlisttest table.

--- config/datastores.js
+++ config/datastores.js
@@ -4,9 +4,9 @@ module.exports.datastores = {
 
   default: {
     adapter: 'sails-mysql',
-    //url: 'mysql://jays@dal-fcs:Test1234@dal-fcs.mysql.database.azure.com:3306/donor_app',
+    url: 'mysql://jays@dal-fcs:Test1234@dal-fcs.mysql.database.azure.com:3306/donor_app',
 	// protocol://user:password@host:port/database
-	url: 'mysql://jonathanh:B00418947@db.cs.dal.ca:3306/jonathanh'
+	// url: 'mysql://jonathanh:B00418947@db.cs.dal.ca:3306/jonathanh'
   },
 
 };
