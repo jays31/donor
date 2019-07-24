f849b13c2170210d827bb8bebffefa675bd01d1e
dinesh sai <b.dineshsai@gmail.com>
2019-07-24 19:20:22 -0300

    changes on sign-up

--- assets/js/validate_api.js
+++ assets/js/validate_api.js
@@ -1,19 +1,35 @@
 // Author: Jay Sharma (B00824331)
 
 function validate(){
-	alert("hai");
+	// alert("hai");
 	if (document.getElementById("password").value!=document.getElementById("confirmPassword").value){
 		alert("Oops! Passwords do not match! Try again!");
 	}
-	else if (document.getElementById("password").value!=''&&document.getElementById("confirmPassword").value!=''&&document.getElementById("orgName").value!=''&&document.getElementById("email").value!=''&&document.getElementById("search_location").value!=''&&document.getElementById("phone").value!='')
+	else if(document.getElementById("password").value!=''&&document.getElementById("confirmPassword").value!=''&&document.getElementById("orgName").value!=''&&document.getElementById("email").value!=''&&document.getElementById("search_location").value!=''&&document.getElementById("phone").value!='')
 	{
-		alert("Thank you for your interest in being a regular donor. We have received your form and will get in touch with you soon. Please look out for an email from us.");
 		var email1=document.getElementById("email").value;
-		var pwd1=document.getElementById("email").value;
-		var addurl = "http://localhost:1337/logins/create?email="+email1+"&password="+pwd1;
-		var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open( "GET", addurl, false ); // false for synchronous request
-		xmlHttp.send(null);	
+		var pwd1=document.getElementById("password").value;
+		
+		var geturl = "http://localhost:1337/logins?email="+email1;
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open( "GET", geturl, false ); // false for synchronous request
+        xmlHttp.send(null);
+        var tdata = xmlHttp.responseText;
+		var parsetdata = $.parseJSON(tdata);
+		var checkemail = parsetdata[0].email;
+		// alert(tdata);
+		// // alert(checkemail);
+		if (checkemail == email1)
+		{
+			alert("Email Already Exists!");
+		}
+		else{
+			alert("Thank you for your interest in being a regular donor. We have received your form and will get in touch with you soon. Please look out for an email from us.");
+			var addurl = "http://localhost:1337/logins/create?email="+email1+"&password="+pwd1;
+			var xmlHttp2 = new XMLHttpRequest();
+			xmlHttp2.open( "GET", addurl, false ); // false for synchronous request
+			xmlHttp2.send(null);	
+		}
 
 
 	}
