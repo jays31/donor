55d00d92a56f8a7dc600e316bc43519bba312fa5
Jonathan.Harris <j.wilfred.harris@gmail.com>
2019-07-27 11:13:24 -0300

    Login failed popup now working with sweetalert.

--- assets/js/login_failed.js
+++ assets/js/login_failed.js
@@ -2,6 +2,13 @@
 
 // https://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-get-parameters
 
+// Include sweetalert script.
+//https://stackoverflow.com/questions/43796705/how-to-include-cdn-in-javascript-file-js
+
+var sweetalert = document.createElement('script');  
+sweetalert.setAttribute('src','https://unpkg.com/sweetalert/dist/sweetalert.min.js');
+document.head.appendChild(sweetalert);
+
 document.body.onload = loginFailed;
 
 function loginFailed(){
@@ -13,19 +20,20 @@ function loginFailed(){
 	
 		// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
 	
-		var loginFailedDiv = document.createElement("div");
+		//var loginFailedDiv = document.createElement("div");
 		
 		// https://v4-alpha.getbootstrap.com/components/alerts/
-		loginFailedDiv.className = "alert alert-danger";
+		//loginFailedDiv.className = "alert alert-danger";
 	  
-		var loginFailedContent = document.createTextNode("Login failed!"); 
+		//var loginFailedContent = document.createTextNode("Login failed!"); 
 	  
-		loginFailedDiv.appendChild(loginFailedContent);  
+		//loginFailedDiv.appendChild(loginFailedContent);  
 
-		var addressTag = document.getElementById("address-tag"); 
+		//var addressTag = document.getElementById("address-tag"); 
 	  
-		document.body.insertBefore(loginFailedDiv, addressTag);
-
+		//document.body.insertBefore(loginFailedDiv, addressTag);
+		
+		swal("Login failed!");
 	}
 	
 }
