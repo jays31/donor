8ec1eddb2f0253a54bfeb2f50151a9c73788e4f7
Jonathan.Harris <j.wilfred.harris@gmail.com>
2019-07-27 10:35:24 -0300

    Committing before checking out master branch

--- assets/js/login_failed.js
+++ assets/js/login_failed.js
@@ -2,12 +2,30 @@
 
 // https://stackoverflow.com/questions/979975/how-to-get-the-value-from-the-get-parameters
 
-var url_string = window.location.href
-var url = new URL(url_string);
-var loginError = url.searchParams.get("loginError");
+document.body.onload = loginFailed;
 
-if(loginError) {
+function loginFailed(){
+	var url_string = window.location.href
+	var url = new URL(url_string);
+	var loginError = url.searchParams.get("loginError");
+
+	if(loginError) {
 	
-	alert("Login failed.");
+		// https://developer.mozilla.org/en-US/docs/Web/API/Document/createElement
+	
+		var loginFailedDiv = document.createElement("div");
+		
+		// https://v4-alpha.getbootstrap.com/components/alerts/
+		loginFailedDiv.className = "alert alert-danger";
+	  
+		var loginFailedContent = document.createTextNode("Login failed!"); 
+	  
+		loginFailedDiv.appendChild(loginFailedContent);  
 
+		var addressTag = document.getElementById("address-tag"); 
+	  
+		document.body.insertBefore(loginFailedDiv, addressTag);
+
+	}
+	
 }
